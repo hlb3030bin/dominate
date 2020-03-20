1ec444b7c6c884e0f9010cbecd37754205786c81
huanglb <hlb@fastfish.com.cn>
2020-03-20 10:25:34 +0800

    提交代码

---
+++ spiderman/src/main/java/avengers/HashedWheelTimerTest.java
@@ -0,0 +1,35 @@
+package avengers;
+
+import io.netty.util.HashedWheelTimer;
+
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @description: TODO
+ * @author: huanglb
+ * @date 2020/1/3 17:15
+ */
+public class HashedWheelTimerTest {
+    public static void main(String[] args) throws IOException {
+        //创建Timer, 精度为100毫秒,
+        HashedWheelTimer timer = new HashedWheelTimer(100, TimeUnit.MILLISECONDS, 16);
+
+        System.out.println(LocalTime.now());
+
+        timer.newTimeout((timeout) -> {
+            System.out.println(LocalTime.now());
+            System.out.println(timeout);
+        }, 5, TimeUnit.SECONDS);
+
+        timer.newTimeout((timeout) -> {
+            System.out.println(LocalTime.now());
+            System.out.println("huangda");
+            System.out.println(timeout);
+        }, 10, TimeUnit.SECONDS);
+
+        //阻塞main线程
+        System.in.read();
+    }
+}
---
+++ spiderman/src/main/java/avengers/spiderman/sss.java
@@ -0,0 +1,18 @@
+package avengers.spiderman;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: TODO
+ * @author: huanglb
+ * @date 2020/2/18 11:19
+ */
+public class sss {
+
+    public static void main(String[] args) {
+        Dog dfog= new Dog("ssdssds","黄色",10010);
+
+        dfog.barkj();
+    }
+}
